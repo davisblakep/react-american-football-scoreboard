17894225425f22ff88f13d72fda529ce277932ad
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-02-26 21:48:39 -0500

    Added sytling and an alert prompt

--- src/App.js
+++ src/App.js
@@ -7,9 +7,21 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, totalHomeScore] = useState(32);
   const [awayScore, totalAwayScore] = useState(32);
+  
+ function touchdown(){
+   alert("Touchdown!")
+ };
 
+ function fieldgoal(){
+  alert("It's Good!")
+}
+
+const body = document.querySelector('body');
+body.style.backgroundImage = "linear-gradient(to right, #3ab5b0 0%, #3d99be 31%, #56317a 100%)";
+  
 
   return (
+    
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
@@ -31,16 +43,26 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => {totalHomeScore(homeScore + 7)}}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => {totalHomeScore(homeScore + 3)}}>Home Field Goal</button>
+     
+          <button className="homeButtons__touchdown" onClick={() => {touchdown();totalHomeScore(homeScore + 7)}}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => {fieldgoal();totalHomeScore(homeScore + 3)}}>Home Field Goal</button>
+          
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => {totalAwayScore(awayScore + 7)}}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => {totalAwayScore(awayScore + 3)}}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => {touchdown();totalAwayScore(awayScore + 7)}}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => {fieldgoal();totalAwayScore(awayScore + 3)}}>Away Field Goal</button>
         </div>
       </section>
     </div>
+
+    
   );
+  
 }
 
+
+  
+
+
 export default App;
+
